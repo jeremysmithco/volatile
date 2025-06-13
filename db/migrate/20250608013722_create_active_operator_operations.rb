d911394a750369db1efe7fb2bd761d3ef2fad109
Jeremy Smith <jeremy@jeremysmith.co>
2025-06-13 13:12:33 -0400

    Update db/migrate/20250608013722_create_active_operator_operations.rb
    Co-authored-by: Kasper Timm Hansen <hey@kaspth.com>

--- db/migrate/20250608013722_create_active_operator_operations.rb
+++ db/migrate/20250608013722_create_active_operator_operations.rb
@@ -2,9 +2,9 @@ class CreateActiveOperatorOperations < ActiveRecord::Migration[8.0]
   def change
     create_table :active_operator_operations do |t|
       t.references :record, polymorphic: true, null: false
-      t.string :name
-      t.integer :version
-      t.json :response
+      t.string :name, null: false
+      t.integer :version, null: false
+      t.json :response, null: false, default: "{}"
       t.datetime :received_at
       t.datetime :processed_at
       t.datetime :failed_at
